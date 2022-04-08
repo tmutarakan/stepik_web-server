c957e48854ff1f9bed751fd3cc66b6e9d58b9732
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-04-08 08:27:33 +0300

    Add task.py

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-#sudo /etc/init.d/gunicorn restart
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/rask.py /etc/gunicorn.d/task.py
+sudo /etc/init.d/gunicorn restart
 #gunicorn --bind='0.0.0.0:8080' hello:wsgi_application
 #sudo /etc/init.d/mysql start
