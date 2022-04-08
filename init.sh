681f5999cf7b9fb90572bf5b273706435ad9df0c
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-04-08 03:30:35 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-gunicorn -b "0.0.0.0:8080" hello:wsgi_application
 #sudo /etc/init.d/mysql start
