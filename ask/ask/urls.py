939fe9126f46cbf1a2657f7cd28dfdea6b810768
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-04-08 08:20:55 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url('', views.test),
     url('login/', views.test),
     url('signup/', views.test),
-    url('question/<int:pk>/', views.test, name='question'),
+    url('question/<int:id>/', views.test),
     url('ask/', views.test),
     url('popular/', views.test),
     url('new/', views.test),
