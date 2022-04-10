b012e8c65e3ad3f5da4c163cd79e5562f4eafcb0
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-04-10 19:15:33 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-#sudo rm -rf /etc/nginx/sites-enabled/default
-#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo /etc/init.d/nginx restart
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 #sudo ln -sf /home/box/web/task.py /etc/gunicorn.d/task.py
