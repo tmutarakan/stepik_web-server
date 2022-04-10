9d316d4fd70bf6bc32e803a9fe5a947c5ec70a48
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-04-10 18:09:34 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_GET
-from qa.models import QuestionManager, Question, Answer
+from qa.models import Question, Answer
 
 
 # Create your views here.
